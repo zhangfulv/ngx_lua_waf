e0a8b6ae4fdbc21b876fa5a682104ee35561ca36
zhangfulv <63481858+zhangfulv@users.noreply.github.com>
2020-12-28 16:37:40 +0800

    修改post校验方式。

--- waf.lua
+++ waf.lua
@@ -1,6 +1,8 @@
 local content_length=tonumber(ngx.req.get_headers()['content-length'])
 local method=ngx.req.get_method()
 local ngxmatch=ngx.re.match
+local cjson = require("cjson");
+
 if whiteip() then
 elseif blockip() then
 elseif denycc() then
@@ -14,73 +16,19 @@ elseif url() then
 elseif args() then
 elseif cookie() then
 elseif PostCheck then
-    if method=="POST" then   
-            local boundary = get_boundary()
-	    if boundary then
-	    local len = string.len
-            local sock, err = ngx.req.socket()
-    	    if not sock then
-					return
-            end
-	    ngx.req.init_body(128 * 1024)
-            sock:settimeout(0)
-	    local content_length = nil
-    	    content_length=tonumber(ngx.req.get_headers()['content-length'])
-    	    local chunk_size = 4096
-            if content_length < chunk_size then
-					chunk_size = content_length
-	    end
-            local size = 0
-	    while size < content_length do
-		local data, err, partial = sock:receive(chunk_size)
-		data = data or partial
-		if not data then
-			return
-		end
-		ngx.req.append_body(data)
-        	if body(data) then
-	   	        return true
-    	    	end
-		size = size + len(data)
-		local m = ngxmatch(data,[[Content-Disposition: form-data;(.+)filename="(.+)\\.(.*)"]],'ijo')
-        	if m then
-            		fileExtCheck(m[3])
-            		filetranslate = true
-        	else
-            		if ngxmatch(data,"Content-Disposition:",'isjo') then
-                		filetranslate = false
-            		end
-            		if filetranslate==false then
-            			if body(data) then
-                    			return true
-                		end
-            		end
-        	end
-		local less = content_length - size
-		if less < chunk_size then
-			chunk_size = less
-		end
-	 end
-	 ngx.req.finish_body()
-    else
-			ngx.req.read_body()
-			local args = ngx.req.get_post_args()
-			if not args then
-				return
+    if method=="POST" then
+		ngx.req.read_body()
+		local args = ngx.req.get_body_data()
+		local boundary = get_boundary()
+		if boundary then
+			for v in string.gmatch(args, '[\n][^C]+') do
+				--testlog("from-:"..s)
+				body(v)
 			end
-			for key, val in pairs(args) do
-				if type(val) == "table" then
-					if type(val[1]) == "boolean" then
-						return
-					end
-					data=table.concat(val, ", ")
-				else
-					data=val
-				end
-				if data and type(data) ~= "boolean" and body(data) then
-                			body(key)
-				end
-			end
+		else
+			-- 解析json格式
+			local json = cjson.decode(args);
+			checkJson(json)
 		end
     end
 else
